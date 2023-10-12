fa1e438e97e8dd865832e3122e258d2af067904a
Meshack <meshack.odunowe@decagon.dev>
2023-10-12 21:53:48 +0100

    new updates

--- src/components/BusinessForm.jsx
+++ src/components/BusinessForm.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-
-  import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const BusinessForm = () => {
-  
   const [formData, setFormData] = useState({
     name: "",
     companyName: "",
@@ -28,49 +26,78 @@ const BusinessForm = () => {
     "Others",
   ];
 
+  const navigate = useNavigate();
+  const [formValid, setFormValid] = useState(true);
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  const url = "https://mozisha-47b2f-default-rtdb.firebaseio.com/talents.json";
 
-  const formDataToSave = {
-    name: formData.name,
-    companyName: formData.companyName,
-    email: formData.email,
-    talentRequirements: formData.talentRequirements,
-    otherRequirements: formData.talentRequirements === "Others" ? formData.otherRequirements : "",
-    numberOfTalents: formData.numberOfTalents,
-    startDate: formData.startDate.toISOString(),
+  const isValidEmail = (email) => {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(email);
   };
 
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formDataToSave),
-    });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    if (response.ok) {
-      toast.success("Form submitted successfully!", {
-        position: "top-right",
-        autoClose: 3000, // Close the toast after 3 seconds
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-      });
-      // Optionally, you can perform the redirection here.
-    } else {
-      console.error("Data could not be saved.");
+    if (
+      formData.name === "" ||
+      formData.companyName === "" ||
+      formData.email === "" ||
+      !isValidEmail(formData.email) ||
+      formData.talentRequirements === ""
+    ) {
+      // If any of these fields are empty or email is invalid, mark the form as invalid
+      setFormValid(false);
+      return;
     }
-  } catch (error) {
-    console.error("Error:", error);
-  }
+
+    // If the form is valid, reset the form validation state
+    setFormValid(true);
+
+    const url = "https://mozisha-47b2f-default-rtdb.firebaseio.com/talents.json";
+
+    const formDataToSave = {
+      name: formData.name,
+      companyName: formData.companyName,
+      email: formData.email,
+      talentRequirements: formData.talentRequirements,
+      otherRequirements:
+        formData.talentRequirements === "Others" ? formData.otherRequirements : "",
+      numberOfTalents: formData.numberOfTalents,
+      startDate: formData.startDate.toISOString(),
+    };
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formDataToSave),
+      });
+
+      if (response.ok) {
+        toast.success("Form submitted successfully!", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+
+        // Redirect to the success page after success
+        navigate("/success");
+      } else {
+        console.error("Data could not be saved.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+
     // Optionally, you can reset the form after submission
     setFormData({
       name: "",
@@ -84,13 +111,14 @@ const handleSubmit = async (e) => {
   };
 
   return (
-
-
-    <div className="mt-4 md:mt-8 h-screen ">
+    <div className="mt-4 md:mt-8 h-screen">
       <h2 className="font-bold text-2xl text-center my-8">
         Talent Request Form
       </h2>
-      <form className="leading-10 h-full md:px-16 ">
+      <form className="leading-10 h-full md:px-16">
+        {formValid === false && (
+          <p className="text-red-500">Please fill in all required fields.</p>
+        )}
         <div>
           <label className="font-bold mr-8">Name:</label>
           <input
@@ -128,7 +156,7 @@ const handleSubmit = async (e) => {
           />
         </div>
         <div className="mt-4 md:mt-8">
-          <label className="font-bold mr-8 w-full">Talent Requirements:</label>
+          <label className="          font-bold mr-8 w-full">Talent Requirements:</label>
           <select
             className="border py-4 px-4 w-full"
             name="talentRequirements"
@@ -181,7 +209,7 @@ const handleSubmit = async (e) => {
         <div className="mt-4 md:mt-8 mb-44">
           <button
             onClick={handleSubmit}
-            className="bg-purple-700 text-white rounded-md md:px-4 md:py-2 py-2  hover:bg-purple-100 mb-4 hover:text-purple-800 hover:font-bold md:w-full justify-center items-center w-full "
+            className="bg-purple-700 text-white rounded-md md:px-4 md:py-2 py-2  hover:bg-purple-100 mb-4 hover:text-purple-800 hover:font-bold md:w-full justify-center items-center w-full"
           >
             Submit
           </button>
@@ -192,3 +220,4 @@ const handleSubmit = async (e) => {
 };
 
 export default BusinessForm;
+
--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -4,22 +4,29 @@ import welcome from "../assets/welcome.jpeg";
 const SuccessPage = () => {
   
   return (
-    <div className="bg-green-50 h-screen my-20">
+    <>
+      <div className="bg-green-50 h-screen my-20 md:px-28 flex flex-col gap-8 ">
+        <div>
+
       <h2 className="text-center text-3xl font-bold text-green-500 py-16">
         Thank you for your submission🤝
       </h2>
+        </div>
       <div className="px-4  object-cover max-w-[500px] rounded-lg mb-8 mx-auto">
         <img src={welcome} alt="welcome" />
       </div>
       <p className="text-purple-600 text-center  font-extrabold">
         We&apos;ll be in touch within the next six hours.
       </p>
-      <div className="mx-auto">
-        <button className="mt-8 bg-purple-600 w-full md:w-fit  py-2 px-10 rounded-md text-purple-200 ">
-          <Link to={"/"}>Back to Homepage </Link>
-        </button>{" "}
+      <div className="mx-auto ">
+          <Link to={"/"} className="bg-purple-600 text-purple-50 py-2 px-4 rounded-lg w-full ">Back to Homepage </Link>
+        
       </div>
-    </div>
+
+        
+      </div>
+      
+    </>
   );
 };
 
